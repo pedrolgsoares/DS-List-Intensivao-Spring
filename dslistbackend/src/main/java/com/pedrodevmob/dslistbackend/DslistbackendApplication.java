77cf9d248d76892ee7186025268b7b3e8b6aca69
Pedro Lucas <pedrodevmob@gmail.com>
2024-11-15 19:38:33 -0300

    FEAT: Adicionando o projeto Spring no backend

---
+++ dslistbackend/src/main/java/com/pedrodevmob/dslistbackend/DslistbackendApplication.java
@@ -0,0 +1,13 @@
+package com.pedrodevmob.dslistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistbackendApplication.class, args);
+	}
+
+}
---
+++ dslistbackend/src/test/java/com/pedrodevmob/dslistbackend/DslistbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrodevmob.dslistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
